d46a1361a1bdbc8aff88785cf81425eedec74024
Mu-munn <rpgmukai333@gmail.com>
2022-12-14 20:54:04 +0900

    dable

--- web/src/App.js
+++ web/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const mukai = 'mukai'
+  const mukai = "mukai"
   return (
     <div className="App">
       <header className="App-header">
