ec9ac318b8829c5817ee4a6dde2ab146611ae4ec
Mu-munn <rpgmukai333@gmail.com>
2022-12-19 15:05:28 +0900

    不要な記述を削除

--- web/components/Layout/UserLayout/index.tsx
+++ web/components/Layout/UserLayout/index.tsx
@@ -1,37 +1,23 @@
 import {
-  Avatar,
   Box,
   Flex,
   forwardRef,
-  Heading,
   HStack,
-  Spacer,
   Stack,
   Text,
-  ToastPositionWithLogical,
   useBreakpointValue,
   useColorModeValue as mode,
   useToast,
-} from "@chakra-ui/react"
-import {
-  BsBuilding,
-  BsHeart,
-  BsLightbulb,
-  BsMailbox,
-  BsPerson,
-  BsSearch,
-  BsShop,
-} from "react-icons/bs"
-import { BiBriefcase, BiCheck, BiCheckboxChecked, BiCheckCircle, BiRocket } from "react-icons/bi"
-import { RiLogoutBoxRLine } from "react-icons/ri"
-import { useRouter } from "next/router"
-import Link from "next/link"
-import { ReactElement, ReactNode, useEffect } from "react"
-// import useAuthUser from "../../src/hooks/useAuthUser"
-import { ScrollArea } from "./ScrollArea"
-import { SidebarLink } from "./SidebarLink"
-import { UserInfo } from "./UserInfo"
-import { supabase } from "../../../src/libs/utils/supabaseClient"
+} from '@chakra-ui/react'
+import { BsBuilding, BsLightbulb, BsMailbox } from 'react-icons/bs'
+import { RiLogoutBoxRLine } from 'react-icons/ri'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import { ReactElement, ReactNode } from 'react'
+import { ScrollArea } from './ScrollArea'
+import { SidebarLink } from './SidebarLink'
+import { UserInfo } from './UserInfo'
+import { supabase } from '../../../src/libs/utils/supabaseClient'
 
 const SidebarLinkComponent = forwardRef(function LogoComponent(
   props: {
@@ -53,8 +39,6 @@ interface AdminLayoutProps {
 
 export const UserLayout = (props: AdminLayoutProps) => {
   const { children } = props
-  //   const { isOpen, toggle } = useMobileMenuState()
-  //   const { isAdmin, email } = useAuthUser()
 
   const router = useRouter()
   const toast = useToast()
@@ -69,74 +53,61 @@ export const UserLayout = (props: AdminLayoutProps) => {
     if (error) {
       alert(JSON.stringify(error))
     } else {
-      router.push("/signIn")
+      router.push('/signIn')
     }
   }
 
   return (
     <Flex
-      height='100vh'
-      bg={mode("blue.800", "gray.800")}
-      overflow='hidden'
-      sx={{ "--sidebar-width": "16rem" }}
+      height="100vh"
+      bg={mode('blue.800', 'gray.800')}
+      overflow="hidden"
+      sx={{ '--sidebar-width': '16rem' }}
     >
       <Box
-        as='nav'
-        display='block'
-        height='100vh'
-        flex='1'
-        width='var(--sidebar-width)'
-        left='0'
-        py='5'
-        px='3'
-        color='gray.200'
-        position='fixed'
+        as="nav"
+        display="block"
+        height="100vh"
+        flex="1"
+        width="var(--sidebar-width)"
+        left="0"
+        py="5"
+        px="3"
+        color="gray.200"
+        position="fixed"
       >
-        <Box fontSize='sm' lineHeight='tall'>
+        <Box fontSize="sm" lineHeight="tall">
           <HStack>
-            {/* <LogoWhite h='6' ml='3' mb='6' mt='2' /> */}
-            <Text fontSize='6' fontWeight='bold' pb='1'>
+            {/* ここにロゴ */}
+            <Text fontSize="6" fontWeight="bold" pb="1">
               {`ダッシュボード`}
             </Text>
           </HStack>
-          <Link href='/profile'>
+          <Link href="/profile">
             <Box
-              as='a'
-              p='3'
-              display='block'
-              transition='background 0.1s'
-              rounded='xl'
-              _hover={{ bg: "whiteAlpha.200" }}
-              whiteSpace='nowrap'
+              as="a"
+              p="3"
+              display="block"
+              transition="background 0.1s"
+              rounded="xl"
+              _hover={{ bg: 'whiteAlpha.200' }}
+              whiteSpace="nowrap"
             >
               <UserInfo />
             </Box>
           </Link>
-          <ScrollArea pt='5' pb='6'>
-            {/* <SidebarLink display={{ base: "block", lg: "none" }} mb='2' icon={<BsSearch />}>
-              Search
-            </SidebarLink> */}
-            <Stack pb='6'>
-              <Link href='/myPage'>
+          <ScrollArea pt="5" pb="6">
+            <Stack pb="6">
+              <Link href="/myPage">
                 <SidebarLinkComponent icon={<BsBuilding />}>自分の活動</SidebarLinkComponent>
               </Link>
-              <Link href='/everyone'>
+              <Link href="/everyone">
                 <SidebarLinkComponent icon={<BsLightbulb />}>みんなの活動</SidebarLinkComponent>
               </Link>
-              {/* <DeployButton /> */}
-
-              {/* <SidebarLink icon={<BsFillBookmarksFill />}>Bookmarks</SidebarLink>
-                  <SidebarLink icon={<BsPencilSquare />}>Drafts</SidebarLink> */}
             </Stack>
-            {/* <Stack pb="6">
-                  <NavSectionTitle>Chats</NavSectionTitle>
-                  <SidebarLink>🎉 Inbox</SidebarLink>
-                  <SidebarLink>👍 Personal</SidebarLink>
-                  <SidebarLink>🦋 Work</SidebarLink>
-                </Stack> */}
-            <Box position='absolute' bottom='8'>
+            <Box position="absolute" bottom="8">
               <SidebarLink
-                mt='2'
+                mt="2"
                 icon={<BsMailbox />}
                 onClick={(e: any) => {
                   //  ここでお問い合わせページに遷移
@@ -144,7 +115,7 @@ export const UserLayout = (props: AdminLayoutProps) => {
               >
                 お問い合わせ
               </SidebarLink>
-              <SidebarLink mt='2' icon={<RiLogoutBoxRLine />} onClick={handleLogOut}>
+              <SidebarLink mt="2" icon={<RiLogoutBoxRLine />} onClick={handleLogOut}>
                 ログアウト
               </SidebarLink>
             </Box>
@@ -152,30 +123,29 @@ export const UserLayout = (props: AdminLayoutProps) => {
         </Box>
       </Box>
       <Box
-        flex='1'
-        p={{ base: "0", md: "6" }}
-        marginStart={{ md: "var(--sidebar-width)" }}
-        position='relative'
-        // left={isOpen ? "var(--sidebar-width)" : "0"}
-        transition='left 0.2s'
+        flex="1"
+        p={{ base: '0', md: '6' }}
+        marginStart={{ md: 'var(--sidebar-width)' }}
+        position="relative"
+        transition="left 0.2s"
       >
         <Box
-          maxW='2560px'
-          bg={mode("white", "gray.700")}
-          height='100%'
-          pb='6'
-          rounded={{ md: "lg" }}
-          overflow='auto'
+          maxW="2560px"
+          bg={mode('white', 'gray.700')}
+          height="100%"
+          pb="6"
+          rounded={{ md: 'lg' }}
+          overflow="auto"
         >
-          <Flex direction='column' height='full'>
-            <Box width='100%' maxW='7xl' py={0} px={5} mx='auto'>
-              <Flex w='full' pt='2' justify='space-between' align='center'>
-                <Flex align='center'>
+          <Flex direction="column" height="full">
+            <Box width="100%" maxW="7xl" py={0} px={5} mx="auto">
+              <Flex w="full" pt="2" justify="space-between" align="center">
+                <Flex align="center">
                   {/* <MobileMenuButton onClick={toggle} isOpen={isOpen} />
                   <NavBreadcrumb /> */}
                 </Flex>
               </Flex>
-              <Box py={{ base: "6" }}>{children}</Box>
+              <Box py={{ base: '6' }}>{children}</Box>
             </Box>
           </Flex>
         </Box>
--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -1,56 +1,9 @@
-import SignUp from "./signUp"
-// import { useSession } from "../src/hooks/useSession"
-import { Center, Container } from "@chakra-ui/react"
-import { Session, useSession, useSupabaseClient, useUser } from "@supabase/auth-helpers-react"
-import { Auth, ThemeSupa } from "@supabase/auth-ui-react"
-import Account from "../components/Account"
-import useAuthUser from "../src/hooks/useAuthUser"
-import router from "next/router"
-import { useEffect } from "react"
-import SignIn from "./signIn"
+import Account from '../components/Account'
+import useAuthUser from '../src/hooks/useAuthUser'
+import SignIn from './signIn'
 
 export default function Home() {
-  // const { user } = useUser()
-  // const session = useSession()
-  // console.log(session)
-
-  const supabase = useSupabaseClient()
-
-  // const session = useSession()
   const { user, isLoading, token } = useAuthUser()
-  // console.log(user)
 
-  // useEffect(() => {
-  //   if (!user && !isLoading) {
-  //     router.push("/signIn")
-  //   }
-  // }, [user, isLoading, router])
-
-  // if (isLoading) {
-  //   return <div>Loading</div>
-  // } else {
-  //   return <Container>{!session}</Container>
-  // }
-
-  return (
-    <>
-      {!user ? (
-        // <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme='dark' />
-        <SignIn />
-      ) : (
-        <Account session={user} />
-      )}
-    </>
-  )
+  return <>{!user ? <SignIn /> : <Account session={user} />}</>
 }
-
-// return (
-//   <>
-//     {!session ? (
-//       // <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} theme='dark' />
-//       <SignUp />
-//     ) : (
-//       <Account session={session} />
-//     )}
-//   </>
-// )
