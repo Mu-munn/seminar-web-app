420613d34a55794627cdc5777ddcfc4d77498158
Mu-munn <rpgmukai333@gmail.com>
2022-12-22 11:06:12 +0900

    準備

--- web/src/libs/corp.ts
+++ web/src/libs/corp.ts
@@ -3,6 +3,8 @@
 import { Corp } from 'src/types/types'
 
 export const sortCorpOfCreated = (corps: Corp[]) => {
+  console.log(corps)
+
   const returnCorp: Corp[] = []
   // ここでcorpsをcreateAtの新しい順に並び替える
   return returnCorp
--- web/src/types/types.ts
+++ web/src/types/types.ts
@@ -8,4 +8,5 @@ export interface Corp {
   corp_id: string
   user_id: string
   corp_name: string
+  update_at: Date
 }
