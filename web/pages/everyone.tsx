fef18cfc349f375ead7019735f58d48d4832cae0
Mu-munn <rpgmukai333@gmail.com>
2023-01-12 12:22:02 +0900

    everyoneをfetchされるように変更

--- web/pages/everyone.tsx
+++ web/pages/everyone.tsx
@@ -1,17 +1,24 @@
 import { UserLayout } from '../components/Layout/UserLayout'
-import { Box, Heading, Stack, Text } from '@chakra-ui/react'
+import { Center, CircularProgress, Heading } from '@chakra-ui/react'
 import { BasicTable } from '@/components/BasicTable'
+import { useEveryOne } from '@/hooks/useEveryOne'
 
 const EveryOne = () => {
+  const everyone = useEveryOne()
+
   return (
-    <>
-      <UserLayout>
-        <Heading pl={4} mb={6} fontSize={'20px'}>
-          みんなの活動
-        </Heading>
-        <BasicTable></BasicTable>
-      </UserLayout>
-    </>
+    <UserLayout>
+      <Heading pl={4} mb={6} fontSize={'20px'}>
+        みんなの活動
+      </Heading>
+      {everyone ? (
+        <BasicTable everyone={everyone} />
+      ) : (
+        <Center w={'100%'} h={'100%'}>
+          <CircularProgress isIndeterminate color="green.300" />
+        </Center>
+      )}
+    </UserLayout>
   )
 }
 
---
+++ web/src/hooks/useEveryOne.ts
@@ -0,0 +1,26 @@
+import { supabase } from '@/libs/utils/supabaseClient'
+import { useEffect, useState } from 'react'
+import { EveryOne } from 'src/types/everyone'
+import { Profile } from 'src/types/types'
+import useProfile from './useProfile'
+
+export const useEveryOne = () => {
+  const { profile } = useProfile()
+  const [everyone, setEveryOne] = useState<any[]>()
+
+  useEffect(() => {
+    profile && fetchEveryOne(profile)
+  }, [profile])
+
+  const fetchEveryOne = async (profile: Profile) => {
+    let { data } = await supabase
+      .from('profiles')
+      .select('id,full_name,class_number,corps(corp_id,corp_name)')
+      .eq('class', profile.class)
+      .eq('grade', profile.grade)
+      .order('class_number')
+    setEveryOne(data as EveryOne[])
+  }
+
+  return everyone
+}
